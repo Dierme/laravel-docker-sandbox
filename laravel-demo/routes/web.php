14fcc8adc383c1248fce41d8e0e0038970066d56
Oleksii Prykhodko <oleksii@cetrez.com>
2020-10-14 23:12:17 +0200

    add a healthcheck route

--- laravel-demo/routes/web.php
+++ laravel-demo/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/healthcheck', function () {
+    return json_encode(["success" => true]);
+});
